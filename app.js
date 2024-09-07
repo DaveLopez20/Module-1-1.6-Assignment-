968c4a9400f1af7d6f448953f35b8398be3ef8eb
DaveLopez20 <lopdav02@gmail.com>
2024-09-06 21:34:43 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("Dave Lopez"); 
-document.write(11+9);
+console.log("hope"); 
+
+console.log("dream")
+
+var name = "dream"; // Display name for constant
+
+const dream = 500; // This is the number of the constant dream
+
+let hope = 1000; // This is the website at full health
+
+hope= hope - 250; // It got hit by a slash from a katana! It has 750 points left!
+
