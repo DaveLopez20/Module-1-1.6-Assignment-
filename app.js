9786110660ff0fff5884fe30a87e1d857f5f93ba
DaveLopez20 <lopdav02@gmail.com>
2024-09-06 23:19:51 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 console.log("hope"); 
 
-console.log("dream")
+var name = hope; // Name for the string and global scope
 
-var name = "dream"; // Display name for constant
+var hope = 1000; // This is the website at full health
 
-const dream = 500; // This is the number of the constant dream
+hope= hope -250; // It got hit by a slash from a katana! It has 750 points left!
 
-let hope = 1000; // This is the website at full health
 
-hope= hope - 250; // It got hit by a slash from a katana! It has 750 points left!
+const dream = 25; // Better than 24
 
+var name = 'dream'; // Name for the constant
+
+var name = 'real'; // This is for block scope
+
+{let real= 500; // Health for block scope only
+
+real= real - 115; // All that is left 385 bits
+}
